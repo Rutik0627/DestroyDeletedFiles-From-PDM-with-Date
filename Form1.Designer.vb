f5d2fa922bef0b8053edfff70f0e8dc43f132bbf
Rutik0627 <96373328+Rutik0627@users.noreply.github.com>
2025-05-07 15:41:17 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.VaultsLabel = New System.Windows.Forms.Label()
+        Me.VaultsComboBox = New System.Windows.Forms.ComboBox()
+        Me.CheckBox_Recursive = New System.Windows.Forms.CheckBox()
+        Me.dtpFromDate = New System.Windows.Forms.DateTimePicker()
+        Me.dtpToDate = New System.Windows.Forms.DateTimePicker()
+        Me.DestroyDeletedItemsButton = New System.Windows.Forms.Button()
+        Me.btnCheckDeletedFiles = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(16, 28)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(293, 72)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "This application destroys all deleted files" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "in the root folder of the selected V" &
+    "ault" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "and also allows you to destroy all deleted" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "files in all of the selected V" &
+    "ault's Subfolders."
+        '
+        'VaultsLabel
+        '
+        Me.VaultsLabel.AutoSize = True
+        Me.VaultsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VaultsLabel.Location = New System.Drawing.Point(21, 101)
+        Me.VaultsLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.VaultsLabel.Name = "VaultsLabel"
+        Me.VaultsLabel.Size = New System.Drawing.Size(127, 20)
+        Me.VaultsLabel.TabIndex = 1
+        Me.VaultsLabel.Text = "Select a Vault:"
+        '
+        'VaultsComboBox
+        '
+        Me.VaultsComboBox.FormattingEnabled = True
+        Me.VaultsComboBox.Items.AddRange(New Object() {"KPCLVault", "KPCLVault_QA"})
+        Me.VaultsComboBox.Location = New System.Drawing.Point(24, 125)
+        Me.VaultsComboBox.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.VaultsComboBox.Name = "VaultsComboBox"
+        Me.VaultsComboBox.Size = New System.Drawing.Size(212, 21)
+        Me.VaultsComboBox.TabIndex = 2
+        '
+        'CheckBox_Recursive
+        '
+        Me.CheckBox_Recursive.AutoSize = True
+        Me.CheckBox_Recursive.Location = New System.Drawing.Point(26, 165)
+        Me.CheckBox_Recursive.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.CheckBox_Recursive.Name = "CheckBox_Recursive"
+        Me.CheckBox_Recursive.Size = New System.Drawing.Size(122, 17)
+        Me.CheckBox_Recursive.TabIndex = 3
+        Me.CheckBox_Recursive.Text = "Include all Subfolder"
+        Me.CheckBox_Recursive.UseVisualStyleBackColor = True
+        '
+        'dtpFromDate
+        '
+        Me.dtpFromDate.Location = New System.Drawing.Point(26, 195)
+        Me.dtpFromDate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.dtpFromDate.Name = "dtpFromDate"
+        Me.dtpFromDate.Size = New System.Drawing.Size(210, 20)
+        Me.dtpFromDate.TabIndex = 4
+        '
+        'dtpToDate
+        '
+        Me.dtpToDate.Location = New System.Drawing.Point(26, 227)
+        Me.dtpToDate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.dtpToDate.Name = "dtpToDate"
+        Me.dtpToDate.Size = New System.Drawing.Size(210, 20)
+        Me.dtpToDate.TabIndex = 5
+        '
+        'DestroyDeletedItemsButton
+        '
+        Me.DestroyDeletedItemsButton.Location = New System.Drawing.Point(24, 263)
+        Me.DestroyDeletedItemsButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.DestroyDeletedItemsButton.Name = "DestroyDeletedItemsButton"
+        Me.DestroyDeletedItemsButton.Size = New System.Drawing.Size(212, 29)
+        Me.DestroyDeletedItemsButton.TabIndex = 6
+        Me.DestroyDeletedItemsButton.Text = "Destroy Deleted Files"
+        Me.DestroyDeletedItemsButton.UseVisualStyleBackColor = True
+        '
+        'btnCheckDeletedFiles
+        '
+        Me.btnCheckDeletedFiles.Location = New System.Drawing.Point(357, 48)
+        Me.btnCheckDeletedFiles.Name = "btnCheckDeletedFiles"
+        Me.btnCheckDeletedFiles.Size = New System.Drawing.Size(257, 40)
+        Me.btnCheckDeletedFiles.TabIndex = 7
+        Me.btnCheckDeletedFiles.Text = "Check Deleted Files"
+        Me.btnCheckDeletedFiles.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(314, 110)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(358, 181)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(708, 367)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnCheckDeletedFiles)
+        Me.Controls.Add(Me.DestroyDeletedItemsButton)
+        Me.Controls.Add(Me.dtpToDate)
+        Me.Controls.Add(Me.dtpFromDate)
+        Me.Controls.Add(Me.CheckBox_Recursive)
+        Me.Controls.Add(Me.VaultsComboBox)
+        Me.Controls.Add(Me.VaultsLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Destroy Deleted files"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents VaultsLabel As Label
+    Friend WithEvents VaultsComboBox As ComboBox
+    Friend WithEvents CheckBox_Recursive As CheckBox
+    Friend WithEvents dtpFromDate As DateTimePicker
+    Friend WithEvents dtpToDate As DateTimePicker
+    Friend WithEvents DestroyDeletedItemsButton As Button
+    Friend WithEvents btnCheckDeletedFiles As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,220 @@
+﻿Imports System.IO
+Imports EdmLib
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            'Declare and create an instance of IEdmVault5
+            'Cast IEdmVault5 to IEdmVault8
+            Dim vault As IEdmVault8 = New EdmVault5
+            Dim Views() As EdmViewInfo = Nothing
+
+            'Populate the form with the names of 
+            'the vaults on the computer
+            vault.GetVaultViews(Views, False)
+            VaultsComboBox.Items.Clear()
+            For Each View As EdmViewInfo In Views
+                VaultsComboBox.Items.Add(View.mbsVaultName)
+            Next
+            If VaultsComboBox.Items.Count > 0 Then
+                VaultsComboBox.Text = VaultsComboBox.Items(0)
+            End If
+
+        Catch ex As Runtime.InteropServices.COMException
+            MessageBox.Show("HRESULT = 0x" + ex.ErrorCode.ToString("X") + vbCrLf + ex.Message)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+
+
+    Private Sub CheckBox_Recursive_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox_Recursive.CheckedChanged
+
+    End Sub
+
+    Private Sub VaultsLabel_Click(sender As Object, e As EventArgs) Handles VaultsLabel.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles dtpFromDate.ValueChanged
+
+    End Sub
+
+    Private Sub DateTimePicker2_ValueChanged(sender As Object, e As EventArgs) Handles dtpToDate.ValueChanged
+
+    End Sub
+
+    Private Sub DestroyDeletedItemsButton_Click(sender As Object, e As EventArgs) Handles DestroyDeletedItemsButton.Click
+        Try
+            'Declare and create an instance of IEdmVault5
+            Dim vault As IEdmVault5 = New EdmVault5()
+
+            'Log into selected vault as the current user
+            vault.LoginAuto(VaultsComboBox.Text, Me.Handle.ToInt32())
+
+            'Check to see if deleted files should be
+            'destroyed in all subfolders
+            Dim aRecursive As Boolean = False
+            If CheckBox_Recursive.Checked Then
+                aRecursive = True
+            End If
+
+            Dim poDeletedItems() As EdmDeletedItems = Nothing
+            Dim ppoDestroyedItems() As EdmFileInfo = Nothing
+            Dim poErrors() As EdmBatchDelErrInfo = Nothing
+            Dim DeletedFolder As IEdmFolder13
+
+            'Get the root folder of the vault
+            DeletedFolder = vault.RootFolder
+
+            DeletedFolder.GetDeletedItems(poDeletedItems, aRecursive)
+
+            DeletedFolder.DestroyDeletedItems2(poDeletedItems, ppoDestroyedItems, poErrors)
+
+            'Destroy all deleted files in the root folder and
+            'subfolders, if the check box is selected on the form
+            Dim idx As Integer
+            idx = LBound(ppoDestroyedItems)
+            Dim msg As String = Nothing
+
+            'If any deleted files found, display their names in a message box
+            While idx <= UBound(ppoDestroyedItems)
+                Dim row As String
+                row = "DocumentID: " + CStr(ppoDestroyedItems(idx).mlFileID) + ", FolderID: " +
+                    CStr(ppoDestroyedItems(idx).mlFolderID) + ", File path: " + ppoDestroyedItems(idx).mbsPath + vbCrLf
+                If ppoDestroyedItems(idx).mhResult = 0 Then
+                    row = row + "Status: OK" + vbCrLf
+                Else
+                    row = row + " status: Failed=" + "HRESULT = 0x" + CStr(poErrors(idx).mlErrorCode)
+                End If
+                idx = idx + 1
+                msg = msg + row + vbCrLf
+            End While
+
+            If msg Is Nothing Then
+                MessageBox.Show("No deleted files were found in the selected vault's root folder or subfolders.")
+            Else
+                MessageBox.Show("The status for each destroyed file is: " + vbCrLf + vbCrLf + msg)
+            End If
+
+        Catch ex As Runtime.InteropServices.COMException
+            MessageBox.Show("HRESULT = 0x" + ex.ErrorCode.ToString("X") + vbCrLf + ex.Message)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+        'get selected dates
+        Dim fromDate As DateTime = dtpFromDate.Value.Date
+        Dim toDate As DateTime = dtpToDate.Value.Date
+
+        ' Ensure "To Date" is not earlier than "From Date"
+        If toDate < fromDate Then
+            MessageBox.Show("Error: 'To Date' cannot be earlier than 'From Date'.", "Invalid Date Range", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' Get the selected folder path
+        Dim folderPath As String = VaultsComboBox.SelectedItem.ToString()
+
+        ' Check if folder exists
+        If Directory.Exists(folderPath) Then
+            ' Loop through each file in the folder
+            For Each file As String In Directory.GetFiles(folderPath)
+                Dim fileInfo As New FileInfo(file)
+                Dim fileDate As DateTime = fileInfo.LastWriteTime ' Get the last modified date
+
+                ' Check if file modification date falls within the selected range
+                If fileDate >= fromDate AndAlso fileDate <= toDate Then
+                    Try
+                        Dim files As Object = Nothing
+                        files.Delete(file)
+                        Console.WriteLine("Deleted: " & file)
+                    Catch ex As Exception
+                        MessageBox.Show("Error deleting file: " & file & vbCrLf & ex.Message, "Delete Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End Try
+                End If
+            Next
+
+            MessageBox.Show("Files deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Selected folder does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+
+    Private Sub btnCheckDeletedFiles_Click(sender As Object, e As EventArgs) Handles btnCheckDeletedFiles.Click
+        ' Validate if a vault is selected
+        If VaultsComboBox.SelectedItem Is Nothing OrElse String.IsNullOrWhiteSpace(VaultsComboBox.SelectedItem.ToString()) Then
+            MessageBox.Show("Please select a valid vault folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' Get the selected folder path
+        Dim folderPath As String = VaultsComboBox.SelectedItem.ToString()
+
+        ' Validate folder existence
+        If Not Directory.Exists(folderPath) Then
+            MessageBox.Show("Selected folder does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' Get selected dates
+        Dim fromDate As DateTime = dtpFromDate.Value.Date
+        Dim toDate As DateTime = dtpToDate.Value.Date
+
+        ' Ensure 'To Date' is not before 'From Date'
+        If toDate < fromDate Then
+            MessageBox.Show("Error: 'To Date' cannot be earlier than 'From Date'.", "Invalid Date Range", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' Check if the "Include Subfolders" checkbox is checked
+        Dim searchOption As SearchOption = If(CheckBox_Recursive.Checked, SearchOption.AllDirectories, SearchOption.TopDirectoryOnly)
+
+        Try
+            ' Get all files in the folder (including subfolders if checked)
+            Dim files As String() = Directory.GetFiles(folderPath, "*.*", searchOption)
+
+            Dim deletedFiles As Integer = 0
+
+            ' Loop through each file and delete if within date range
+            For Each file As String In files
+                Dim fileInfo As New FileInfo(file)
+                Dim fileDate As DateTime = fileInfo.LastWriteTime ' Get the last modified date
+
+                'Dim Vfile As String
+                ' Dim filepath As String
+                'filepath = "C:\Vaults\KPCLVault_QA"
+
+                ' Check if file modification date falls within the selected range
+                If fileDate >= fromDate AndAlso fileDate <= toDate Then
+                    Try
+                        ' Vfile.Delete(filepath)
+                        deletedFiles += 1
+                    Catch ex As UnauthorizedAccessException
+                        MessageBox.Show("Access denied: " & file, "Permission Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    Catch ex As IOException
+                        MessageBox.Show("File in use: " & file, "File Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    End Try
+                End If
+            Next
+
+            ' Show success message
+            MessageBox.Show(deletedFiles & " files deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+End Class
